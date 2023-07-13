8d4072c405395f9bb5d181ae07ab9e031da504a6
pdunker <pkdunker@gmail.com>
2023-07-12 22:40:39 -0300

    Project CalcPKD created

---
+++ CalcPKD/CalcPKD/CalcPKDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcPKDApp.swift
+//  CalcPKD
+//
+//  Created by Philip Dunker on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CalcPKDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
