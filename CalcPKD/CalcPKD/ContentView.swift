fe834d6061634a6b7d8b867a1abcf664ddcf9f12
pdunker <pkdunker@gmail.com>
2023-07-16 15:03:08 -0300

    Created CalcController and Code cleanup

--- CalcPKD/CalcPKD/ContentView.swift
+++ CalcPKD/CalcPKD/ContentView.swift
@@ -8,27 +8,33 @@
 import SwiftUI
 import UIKit
 
-struct ContentView: View {
-    var body: some View {
-        StoryBoardView().edgesIgnoringSafeArea(.all)
+struct ContentView: View
+{
+    var body: some View
+    {
+        StoryBoardView()//.edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
 
-//Using StoryBoard In SwiftUI How To Embed StoryBoard In SwiftUI How To Use StoryBoard In SwiftUI
-//https://www.youtube.com/watch?v=T8_uqibv0YI
-struct StoryBoardView: UIViewControllerRepresentable{
-    func makeUIViewController(context: Context) -> some UIViewController {
+// Using StoryBoard In SwiftUI How To Embed StoryBoard In SwiftUI How To Use StoryBoard In SwiftUI
+// https://www.youtube.com/watch?v=T8_uqibv0YI
+struct StoryBoardView: UIViewControllerRepresentable
+{
+    func makeUIViewController(context: Context) -> some UIViewController
+    {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let controller = storyboard.instantiateViewController(identifier: "Home")
         return controller
     }
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context)
+    {
     }
 }
